d39eb4abe401e12f10157e5ba4cdb1a1163d55e0
nor0x <nor0x@users.noreply.github.com>
2024-09-10 15:31:16 +0200

    added suggestions

--- WahlGPT.Web/wwwroot/js/app.js
+++ WahlGPT.Web/wwwroot/js/app.js
@@ -1,8 +1,6 @@
 ﻿window.scrollToElement = function (selector) {
 	var element = document.querySelector(selector);
 	if (element) {
-		//animate height of #one to 200px
-
 		let one = document.getElementById('one');
 		one.style.height = 'auto';
 		one.style.opacity = '1';
@@ -18,3 +16,47 @@ window.copyToClipboard = function (email) {
 	alert('Email Adresse kopiert: ' + email);
 }
 
+let suggestions = [
+	"Was wird für Umweltschutz getan?",
+	"Wie ist die Position zu Klimaschutz?",
+	"Wie wird die Digitalisierung vorangetrieben?",
+	"Wie steht die Partei zum Thema Bildung?",
+	"Wie können Familien finanziell unterstützt werden?",
+	"Gibt es Pläne für die Gesundheitsversorgung?",
+	"Welche Pläne gibt es für die Pensionen?",
+	"Wie ist die Position zu sozialen Themen?",
+	"Welche Pläne gibt es für die Wirtschaft?",
+	"Wie können Startups unterstützt werden?",
+	"Wie ist die Position zu Europa und EU?",
+	"Wie steht die Partei zu Migration?",
+	"Welche Pläne gibt es für die Landwirtschaft?",
+	"Wie ist die Position zu Sicherheit und Polizei?",
+];
+
+var questionInput = undefined;
+window.doSuggestions = function () {
+	questionInput = document.querySelector('.question-input');
+	question = questionInput.value;
+	if (question == '') {
+		let placeholder = document.querySelector('.question-input');
+		let index = Math.floor(Math.random() * suggestions.length);
+		typeWriter(placeholder, suggestions[index], 0);
+
+		setInterval(function () {
+			if (questionInput && questionInput.value == '') {
+				let placeholder = document.querySelector('.question-input');
+				let index = Math.floor(Math.random() * suggestions.length);
+				typeWriter(placeholder, suggestions[index], 0);
+			}
+		}, Math.floor(Math.random() * 3000) + 3000);
+	}
+}
+
+let typeWriter = function (element, text, i) {
+	if (i < text.length) {
+		element.setAttribute('placeholder', text.substring(0, i + 1));
+		setTimeout(function () {
+			typeWriter(element, text, i + 1)
+		}, Math.floor(Math.random() * 90) + 10);
+	}
+}
